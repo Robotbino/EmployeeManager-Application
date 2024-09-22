9440479f30e7cb6ec3bdff2cbd413586686a0e6d
Robotbino <hlongwanabino@gmail.com>
2024-09-22 06:43:20 +0200

    Update Employee.java
    Added attributes to Employee class

--- src/main/java/com/EmployeeManager/Application/Models/Employee.java
+++ src/main/java/com/EmployeeManager/Application/Models/Employee.java
@@ -1,4 +1,24 @@
 package com.EmployeeManager.Application.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Employee {
+    public Employee(){}
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false)
+    public Long id;
+    String Fname;
+    String Lname;
+    String email;
+    String jobTitle;
+    String imageUrl;
+    String mobilenumber;
+
+
 }
