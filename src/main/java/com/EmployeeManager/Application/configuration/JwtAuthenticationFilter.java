26384807d729ed2f9237555ee16ec578abe6dbaa
Robotbino <hlongwanabino@gmail.com>
2024-10-06 21:02:13 +0200

    Security Config and Authentication filter

--- src/main/java/com/EmployeeManager/Application/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/EmployeeManager/Application/configuration/JwtAuthenticationFilter.java
@@ -1,4 +1,43 @@
 package com.EmployeeManager.Application.configuration;
 
-public class JwtAuthenticationFilter {
+import com.EmployeeManager.Application.Services.JwtService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+//This is where we are going to be building the Authentication filter and  specifying its behaviours
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void doFilterInternal(
+            @NonNull
+            //Our request, we can intercept data from the request and send it back in the response
+            HttpServletRequest request,
+            @NonNull
+            //Presenting new data in the response.
+            HttpServletResponse response,
+            @NonNull
+            FilterChain filterChain
+            //The chain of responsibility design-pattern contains a list of all the other filter
+
+    ) throws ServletException, IOException{
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        
+    }
 }
--- src/main/java/com/EmployeeManager/Application/configuration/SecurityConfiguration.java
+++ src/main/java/com/EmployeeManager/Application/configuration/SecurityConfiguration.java
@@ -1,4 +1,48 @@
 package com.EmployeeManager.Application.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private JwtAuthenticationFilter jwtAuthFilter;
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        //Everything that I have in here are authentication related methods
+                        .requestMatchers(POST,"/api/v1/auth/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+//    "/api/v1/auth/**"
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
